57db56d418fd1323f0b361c43755ba2f7003d539
ueni.tech <ueni.tech@gmail.com>
2024-02-11 17:21:51 +0900

    作品ごとのレビューを取得するapiを設定

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    /**
+     * レビューを全件取得
+     * 
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index($media_type, $media_id): \Illuminate\Http\JsonResponse
+    {
+        $reviews = Review::with('user')
+            ->where('media_type', $media_type)
+            ->where('media_id', $media_id)
+            ->get();
+        return response()->json($reviews);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Review $review)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Review $review)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Review $review)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Review $review)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ReviewController;
+use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/reviews/{media_type}/{media_id}', [ReviewController::class, 'index']);
